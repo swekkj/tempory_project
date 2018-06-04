05f4ab687c04cc85c1fd6d00766cd1e588b6257a
jeongwon <kjwon8202002@gmail.com>
2018-06-04 21:26:15 +0900

    jw add link

--- routes/order.js
+++ routes/order.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('order', { title: 'Express',test:"order" });
 });
 
+router.get('/detail/:idx',function(req,res,next){
+  pool.getConnection(function(err,conn){
+    if(err) console.error("poll connection error : " + err);
+    var idx = req.params.idx;
+    var sql = "select * from game where idx=?";
+    conn.query(sql,[idx],function(err, rows){
+      res.render('detail',{rows:rows});
+    });
+  });
+});
+
 module.exports = router;
