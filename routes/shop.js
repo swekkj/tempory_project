c8b2fc1730e4a52bdd72f1f1ed9f1007009a9dd1
jeongwon <kjwon8202002@gmail.com>
2018-06-05 19:29:17 +0900

    jw pass edit

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'chlwodms*1',
+  password: 'root',
   database: 'swe',
   connectionLimit: 5,
 });
