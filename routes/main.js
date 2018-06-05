680aedbd8cb5933a20979acfd093b95f524099bf
CJE <jaeen1113@naver.com>
2018-06-05 20:21:14 +0900

    cje detail good

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: 'chlwodms*1',
   database: 'swe',
   connectionLimit: 5,
 });
