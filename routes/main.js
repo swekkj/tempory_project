b10b301baf8d775c051d2bfb81bca9578fea30e9
dinohee <kkrs0512@naver.com>
2018-06-05 19:56:58 +0900

    temp

---
+++ routes/main.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'PASSWORD',
+  database: 'swe',
+  connectionLimit: 5,
+});
+/* GET home page. */
+router.post('/',function(req,res,next){
+    var username = req.body.username;
+    var cmp = req.body.password;
+    console.log(username);
+    console.log(cmp);
+
+    pool.getConnection(function(err, conn){
+      var sql = "SELECT passwd FROM user_data where id=?";
+      conn.query(sql,[username], function(err, result){
+        if(err) console.error("find user err : "+err);
+        if(result.length==0) res.send("not user");
+        else if(cmp!=result[0].passwd)  // passwd 찾았고 다름
+        {res.send("passwd error");}
+        else
+        {
+          conn.query("SELECT * FROM game", function(err,game_rows){
+            if(err) console.error("game list query error : " + err);
+            conn.query("SELECT * FROM bucket",function(err,bucket_rows)
+            {
+              if(err) console.error("bucket query error : "+err);
+              res.render('login_shop',{title: 'Shop', rows: game_rows, user:username, bucket:bucket_rows});
+              conn.release();
+            });
+          });
+        }
+    });
+  });
+});
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -9,35 +9,6 @@ var pool = mysql.createPool({
   database: 'swe',
   connectionLimit: 5,
 });
-/* GET home page. */
-router.post('/',function(req,res,next){
-    var username = req.body.username;
-    var cmp = req.body.password;
-    console.log(username);
-    console.log(cmp);
-
-    pool.getConnection(function(err, conn){
-      var sql = "SELECT passwd FROM user_data where id=?";
-      conn.query(sql,[username], function(err, result){
-        if(err) console.error("find user err : "+err);
-        if(result.length==0) res.send("not user");
-        else if(cmp!=result[0].passwd)  // passwd 찾았고 다름
-        {res.send("passwd error");}
-        else
-        {
-          conn.query("SELECT * FROM game", function(err,game_rows){
-            if(err) console.error("game list query error : " + err);
-            conn.query("SELECT * FROM bucket",function(err,bucket_rows)
-            {
-              if(err) console.error("bucket query error : "+err);
-              res.render('login_shop',{title: 'Shop', rows: game_rows, user:username, bucket:bucket_rows});
-              conn.release();
-            });
-          });
-        }
-    });
-  });
-});
 router.get('/', function(req, res, next) {
   pool.getConnection(function(err,conn){
     if(err) console.error("poll connection error : " + err);
